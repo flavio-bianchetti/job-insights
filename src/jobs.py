539395f55148d25d9fbfbf61dedba3ad493858ea
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-01 16:13:47 -0300

    feat: read function implemented

--- src/jobs.py
+++ src/jobs.py
@@ -1,9 +1,9 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-
     """Reads a file from a given path and returns its contents
 
     Parameters
@@ -16,4 +16,13 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    try:
+        with open(path, encoding='utf-8') as file:
+            # https://stackoverflow.com/questions/21572175/convert-csv-file-to-list-of-dictionaries
+            result = [
+                {key: value for key, value in row.items()}
+                for row in csv.DictReader(file, skipinitialspace=True)
+            ]
+            return result
+    except FileNotFoundError:
+        raise FileNotFoundError("File not found")
