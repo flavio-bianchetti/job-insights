3eabd082f64c4531bb232665db257f1faf8bc410
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-03 19:16:35 -0300

    feat: count_ocurrences test function implemented

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
 # from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    words = ["python", "javascript"]
+    expected = [1639, 122]
+    result = []
+    for word in words:
+        result.append(count_ocurrences("src/jobs.csv", word))
+    assert all((value in expected) for value in result)
