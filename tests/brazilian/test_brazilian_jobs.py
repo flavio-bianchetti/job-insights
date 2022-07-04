2789c803bb3250b9a611340b09cb2305398db66f
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-04 17:21:12 -0300

    feat: read_brazilian_file test function implemented

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
 # from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs_list = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    jobs_keys = set(key for dict in jobs_list for key in dict.keys())
+    expected = ["title", "salary", "type"]
+    assert all((key in expected) for key in jobs_keys)
